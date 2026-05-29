c87054ed41fdc480e8be370c5f959def97c71be5
kridayslifestyle <kridayslifestyle@gmail.com>
2026-05-29 16:24:21 +0530

    fix callback build

--- src/app/auth/meta/callback/page.tsx
+++ src/app/auth/meta/callback/page.tsx
@@ -1,16 +1,7 @@
-"use client";
-
-import { useSearchParams } from "next/navigation";
-
 export default function MetaCallbackPage() {
-  const searchParams = useSearchParams();
-
-  const code = searchParams.get("code");
-
   return (
     <div>
       <h1>Meta Callback</h1>
-      <p>Code: {code}</p>
     </div>
   );
 }
